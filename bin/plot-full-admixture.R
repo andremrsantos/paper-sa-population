ddca864c173d913026ecb6c81380be82aaea3df0
André Ribeiro dos Santos <andremr@MacBook-Pro.local>
2020-09-10 14:40:38 -0400

    Update plot-full-admixture.R to produce diagnostic plots

--- bin/plot-full-admixture.R
+++ bin/plot-full-admixture.R
@@ -14,19 +14,44 @@ fam <- here("out", "plink", "DataA.fam") %>%
   read_fam() %>%
   left_join(read_sample_info())
 
-nms <- sprintf("K=%d", 2:10)
-dat <- here("out", "admixture", "DataA.%d.Q") %>%
-  sprintf(2:10) %>%
-  set_names(nms) %>%
-  map_dfr(read_q, fam, subgroup2, .id = "K") %>%
-  mutate(K = parse_factor(K, nms))
+dat <- tibble(Ks = 2:10, K = factor(Ks, labels = sprintf("K=%d", Ks))) %>%
+  mutate(
+    q = here("out", "admixture", "DataA.%d.Q") %>%
+      sprintf(Ks) %>%
+      map(read_q, fam, subgroup2),
+    log = here("out", "admixture", "admixture-K%d.log") %>%
+      sprintf(Ks) %>%
+      map(read_log)
+  ) %>%
+  unnest(c(q, log))
 
 ## plot full distruct
-full_distruct <- plot_distruct(dat, pop, subgroup2, K = K) +
+full_distruct <- plot_distruct(select(dat, -Ks), pop, subgroup2, K = K) +
   scale_fill_manual(values = cbpal) +
   facet_grid(K~.)
 
 save_plot(
   here("figs", "sup-fig", "sup-fig_admixture-full"),
+  full_distruct, width = 10, height = 8
+)
+
+full_diagnostic <- dat %>%
+  distinct(Ks, cv_error, likelihood) %>%
+  rename(`CV Error` = cv_error, Likelihood = likelihood) %>%
+  pivot_longer(-Ks) %>%
+  ggplot(aes(Ks, value)) +
+  geom_line() +
+  geom_point() +
+  facet_grid(name~., scales = "free", switch = "both") +
+  scale_y_continuous("") +
+  scale_x_continuous("Puntative Ancestral Components (K)", breaks = 2:10) +
+  theme(
+    strip.text.y = element_text(angle = 0),
+    strip.background = element_blank(),
+    strip.placement = "outside"
+  )
+
+save_plot(
+  here("figs", "sup-fig", "sup-fig_admixture-diagnostic"),
   full_distruct, width = 10, height = 8
 )
