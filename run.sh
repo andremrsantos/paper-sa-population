7bbc43aa6939569a3ef83a176f8a6f77519a030a
André Ribeiro dos Santos <andremr@MacBook-Pro.local>
2020-09-09 22:49:59 -0400

    Added run.sh to execute all individual analysis scripts

---
+++ run.sh
@@ -0,0 +1,10 @@
+Rscript --vanilla bin/plot-fig1.R
+Rscript --vanilla bin/plot-fig2.R
+Rscript --vanilla bin/plot-fig3-4.R
+
+Rscript --vanilla bin/plot-full-admixture.R
+Rscript --vanilla bin/plot-PCAs.R
+Rscript --vanilla bin/run-f3.R
+Rscript --vanilla bin/run-qpGraph.R
+Rscript --vanilla bin/run-ROH.R
+Rscript --vanilla bin/run-treemix.R
