49cc268c7f3086558454a2a723fab5af75712d57
André Ribeiro dos Santos <andremr@MacBook-Pro.local>
2020-09-10 14:36:33 -0400

    Move installation of requirements to its own script

--- R/setup.R
+++ R/setup.R
@@ -1,25 +1,9 @@
 set.seed(551991)
 
-## Install requirements
-installed_packages <- rownames(installed.packages())
-required <- c(
-  "tidyverse", "here", "furrr", "patchwork", "ggforce", "ggrepel", "ggbeeswarm",
-  "tidygraph", "ggraph", "remotes", "Rcpp", "RcppArmadillo", "matrixStats",
-  "sf", "rnaturalearth", "sp", "gstat", "raster", "sys", "ape"
-)
-install.packages(required[!required %in% installed_packages])
-## flashpcaR
-if (!"flashpcaR" %in% installed_packages)
-  remotes::install_github("gabraham/flashpca/flashpcaR")
-## admixtools
-if (!"admixtools" %in% installed_packages)
-  remotes::install_github("uqrmaie1/admixtools")
-## ggtree
-if (!"ggtree" %in% installed_packages)
-  BiocManager::install("ggtree")
-
 ## Wrapper to save both PDF and TIFF
 save_plot <- function(prefix, plot, ...) {
+  dir.create(dirname(prefix), showWarnings = FALSE, recursive = TRUE)
+  
   ggsave(paste0(prefix, ".pdf"), plot, ..., useDingbats = FALSE)
   ggsave(
     paste0(prefix, ".tiff"), plot, ...,
---
+++ bin/00_install_requirements.R
@@ -0,0 +1,19 @@
+#! /usr/bin/env Rscript --vanilla
+
+## Install requirements
+installed_packages <- rownames(installed.packages())
+required <- c(
+  "tidyverse", "here", "furrr", "patchwork", "ggforce", "ggrepel", "ggbeeswarm",
+  "tidygraph", "ggraph", "remotes", "Rcpp", "RcppArmadillo", "matrixStats",
+  "sf", "rnaturalearth", "sp", "gstat", "raster", "sys", "ape"
+)
+install.packages(required[!required %in% installed_packages])
+## flashpcaR
+if (!"flashpcaR" %in% installed_packages)
+  remotes::install_github("gabraham/flashpca/flashpcaR")
+## admixtools
+if (!"admixtools" %in% installed_packages)
+  remotes::install_github("uqrmaie1/admixtools")
+## ggtree
+if (!"ggtree" %in% installed_packages)
+  BiocManager::install("ggtree")
