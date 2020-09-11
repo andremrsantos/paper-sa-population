ef23ff934b17aac5658e0f39b4e16425021f6005
André Ribeiro dos Santos <andremr@MacBook-Pro.local>
2020-09-11 11:37:52 -0400

    updated ROH plots and consolidate code

--- bin/run-ROH.R
+++ bin/run-ROH.R
@@ -1,9 +1,12 @@
-library(tidyverse)
-library(here)
-library(patchwork)
+suppressMessages({
+  library(tidyverse)
+  library(here)
+  library(patchwork)
 
-source(here("R", "setup.R"))
-source(here("R", "sample.R"))
+  source(here("R", "setup.R"))
+  source(here("R", "plink.R"))
+  source(here("R", "sample.R"))
+})
 
 dir.create(here("figs", "sup-fig"), recursive = TRUE, showWarnings = FALSE)
 
@@ -28,25 +31,69 @@ if (!file.exists(here("out", "roh", "DataB.hom"))) {
 summarise_roh <- function(dat, ...) {
   dat %>%
     dplyr::group_by(IID, ...) %>%
-    dplyr::summarise(total_length = sum(KB)/1e3, rohs = n()) 
+    dplyr::summarise(total_length = sum(KB)/1e3, rohs = n(), .groups =  "drop") 
 }
 
-legend_style <- function(at = c(1, 1), color = 1, shape = 2) {
-  list(
-    ggplot2::guides(
-      color = guide_legend(order = color),
-      shape = guide_legend(order = shape)
-    ),
-    ggplot2::theme(
-      legend.position = at,
-      legend.justification = at,
-      legend.background = element_blank(),
-      legend.box = "horizontal",
-      legend.title = element_text(size = 5), 
-      legend.text = element_text(size = 5),
-      legend.key.height = unit(.75, "lines")
+# legend_style <- function(at = c(1, 1), color = 1, shape = 2) {
+#   list(
+#     ggplot2::guides(
+#       color = guide_legend(order = color),
+#       shape = guide_legend(order = shape)
+#     ),
+#     ggplot2::theme(
+#       legend.position = at,
+#       legend.justification = at,
+#       legend.background = element_blank(),
+#       legend.box = "horizontal",
+#       legend.title = element_text(size = 5), 
+#       legend.text = element_text(size = 5),
+#       legend.key.height = unit(.75, "lines")
+#     )
+#   )
+# }
+
+plot_roh <- function(dat, shape) {
+  ## Define default setup
+  plot_pointrange <- list(
+    stat_summary(fun.data = median_hilow, position = position_dodge(width = .8), geom = "linerange"),
+    stat_summary(fun = median, position = position_dodge(width = .8), geom = "point", size = 1.5)
+  )
+  plot_scatter <- geom_point(size = 1.5)
+
+  plot_style <- list(
+    scale_shape_manual("Sample Age", values = 1:5 + 20),
+    scale_color_manual("Region", values = subgroup_palette),
+    theme(
+      strip.text.y = element_text(angle = 0, hjust = 0),
+      strip.background = element_blank()
     )
   )
+
+  roh_pop <- summarise_roh(roh, {{shape}}, pop, subgroup) %>%
+    ggplot(aes(total_length, pop, shape = {{shape}}, color = subgroup)) +
+    theme_classic(8, "Helvetica") +
+    plot_pointrange +
+    plot_style +
+    facet_grid(subgroup~., scales = "free", space = "free") +
+    labs(y = "Population", x = "Total ROH length (Mbp)")
+  
+  roh_length <- summarise_roh(roh, {{shape}}, subgroup, length) %>%
+    ggplot(aes(length, total_length, color = subgroup, shape = {{shape}})) +
+    plot_pointrange +
+    plot_style +
+    guides(shape = "none", color = "none") +
+    labs(x = "ROH length category (Mbp)", y = "Total ROH length (Mbp)")
+
+  roh_scatter <- summarise_roh(roh, {{shape}}, subgroup) %>%
+    ggplot(aes(total_length, rohs, color = subgroup, shape = {{shape}})) +
+    plot_scatter +
+    plot_style +
+    guides(shape = "none", color = "none") +
+    labs(x = "Total ROH length (Mbp)", y = "Total number of ROHs")
+  
+  ((roh_length / roh_scatter) | roh_pop) +
+    plot_layout(guides = "collect") +
+    plot_annotation(tag_levels = "A")
 }
 
 ## Load data -----
@@ -62,10 +109,6 @@ subgroup_palette <- c(
   set_names(setdiff(cbpal, subgroups_pal)[1:3], subgroup_subset[1:3])
 )
 
-#   "#ffffb3", "#bebada", "#fb8072", "#80b1d3", "#fdb462",
-#   "#b3de69", "#fccde5", "#d9d9d9", "#bc80bd", "#ccebc5", "#ffed6f"
-# )
-
 roh <- here("out", "roh", "DataB.hom") %>%
   read_table2(col_types = "ccdicciididdd") %>%
   left_join(read_sample_info(), by = c(IID = "sample")) %>%
@@ -76,71 +119,14 @@ roh <- here("out", "roh", "DataB.hom") %>%
 
 ## Plots -------
 ## A) ROH by age
-roh_age <- summarise_roh(roh, age, subgroup, length) %>%
-  ggplot(aes(length,total_length, color = subgroup, shape = age)) +
-  stat_summary(fun.data = median_hilow, position = position_dodge(width = .8)) +
-  scale_shape_manual(values = c(21, 22)) +
-  scale_color_manual(values = subgroup_palette) +
-  labs(
-    x = "ROH length category (Mbp)",
-    y = "Total ROH length (Mbp)",
-    shape = "Sample Age",
-    color = "Region"
-  ) +
-  legend_style(color = 2, shape = 1)
-
-roh_age_scatter <- summarise_roh(roh, age, subgroup) %>%
-  ggplot2::ggplot(aes(total_length, rohs, color = subgroup, shape = age)) +
-  ggplot2::geom_point() +
-  ggplot2::scale_shape_manual(values = c(21, 22)) +
-  ggplot2::scale_color_manual(values = subgroup_palette) +
-  ggplot2::labs(
-    x = "Total ROH length (Mbp)",
-    y = "Total number of ROHs",
-    shape = "Sample Age",
-    color = "Region"
-  ) +
-  legend_style(c(0, 1))
-
 save_plot(
   here("figs", "sup-fig", "sup-fig_roh-age"),
-  (roh_age + roh_age_scatter) + plot_annotation(tag_levels = "A"),
-  width = 8, height = 4
+  plot_roh(roh, age), width = 8, height = 10
 )
 
 ## B) ROH Study
-roh_study <- roh %>%
-  filter(age == "Contemporan") %>%
-  summarise_roh(study, subgroup, length) %>%
-  ggplot(aes(length, total_length, color = subgroup, shape = study)) +
-  stat_summary(fun.data = median_hilow, position = position_dodge(width = .8)) +
-  scale_shape_manual(values = 1:5 + 20) +
-  scale_color_manual(values = subgroup_palette) +
-  labs(
-    x = "ROH length category (Mbp)",
-    y = "Total ROH length (Mbp)",
-    shape = "Study",
-    color = "Region"
-  ) +
-  legend_style(color = 2, shape = 1)
-
-roh_study_scatter <- roh %>%
-  filter(age == "Contemporan") %>%
-  summarise_roh(study, subgroup) %>%
-  ggplot(aes(total_length, rohs, color = subgroup, shape = study)) +
-  geom_point() +
-  scale_shape_manual(values = 1:5 + 20) +
-  scale_color_manual(values = subgroup_palette) +
-  labs(
-    x = "Total ROH length (Mbp)",
-    y = "Total number of ROHs",
-    shape = "Sample Age",
-    color = "Region"
-  ) +
-  legend_style(c(0, 1))
-
 save_plot(
   here("figs", "sup-fig", "sup-fig_roh-study"),
-  (roh_study + roh_study_scatter) + plot_annotation(tag_levels = "A"),
-  width = 8, height = 4
+  plot_roh(filter(roh, age == "Contemporan"), age),
+  width = 8, height = 10
 )
