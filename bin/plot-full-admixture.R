cf0e26324811a850db7fb72d9006c033558a271c
André Ribeiro dos Santos <andremr@MacBook-Pro.local>
2020-09-11 16:59:13 -0400

    added vertical version of distruct plot

--- bin/plot-full-admixture.R
+++ bin/plot-full-admixture.R
@@ -34,13 +34,34 @@ dat <- tibble(Ks = 2:10, K = factor(Ks, labels = sprintf("K=%d", Ks))) %>%
   unnest(c(q, log))
 
 ## plot full distruct
-full_distruct <- plot_distruct(select(dat, -Ks), pop, subgroup2, K = K) +
+distruct <- plot_distruct(select(dat, -Ks), pop, subgroup2, K = K) +
   scale_fill_manual(values = cbpal) +
-  facet_grid(K~.)
+  facet_grid(K~subgroup2, scales = "free", space = "free")
 
+full_distruct <- distruct +
+  facet_grid(K~subgroup2, scales = "free", space = "free") +
+  theme(
+    axis.text.x = element_text(angle = 90, hjust = 1, size = 5),
+    strip.text.x = element_text(angle = 90, hjust = 0, size = 8),
+    strip.background.x = element_blank()
+  )
 save_plot(
   here("figs", "sup-fig", "sup-fig_admixture-full"),
-  full_distruct, width = 10, height = 8
+  strip_clip_off(full_distruct, "t"), width = 10, height = 8
+)
+
+vert_distruct <- distruct +
+  facet_grid(subgroup2~K, scales = "free", space = "free") +
+  coord_flip() +
+  theme(
+    axis.text.y = element_text(size = 5),
+    strip.text.y = element_text(angle = 0, hjust = 0, size = 8),
+    strip.background.y = element_blank(),
+    panel.spacing.x = unit(.4, "lines")
+  )
+save_plot(
+  here("figs", "sup-fig", "sup-fig_admixture-vertical"),
+  strip_clip_off(vert_distruct, "r"), width = 8, height = 11
 )
 
 full_diagnostic <- dat %>%
