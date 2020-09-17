cf40f52953e6067ac41c5346e20789eac1f0575e
André Ribeiro dos Santos <andremr@MacBook-Pro.local>
2020-09-17 14:36:58 -0400

    added CLI

--- bin/plot-sup-fig_PCA.R
+++ bin/plot-sup-fig_PCA.R
@@ -1,27 +1,32 @@
-library(tidyverse)
-library(here)
-library(patchwork)
+cli::cli_h1("Plotting Sup. Figure 4-6 - PCA")
+cli::cli_alert_info("Setup environment")
 
-source(here("R", "setup.R"))
-source(here("R", "sample.R"))
-source(here("R", "plink.R"))
-source(here("R", "pca.R"))
+suppressMessages({
+  library(tidyverse)
+  library(here)
+  library(patchwork)
 
-## Setup
-theme_set(theme_classic(10, "Helvetica"))
+  source(here("R", "setup.R"))
+  source(here("R", "sample.R"))
+  source(here("R", "plink.R"))
+  source(here("R", "pca.R"))
+})
 
 dir.create(here("figs", "sup-fig"), recursive = TRUE, showWarnings = FALSE)
+
 ## Load
+cli::cli_alert_info("Load data")
 dat <- read_plink(here("out", "plink", "DataB"))
-dat$fam <- left_join(dat$fam, read_sample_info())
+fam <- left_join(dat$fam, read_sample_info())
 
 ## Amazon PCA
+cli::cli_alert_info("Generating Sup Figure 4 - Amazon")
 this_pal <- c(
   "#999999", "#E69F00", "#56B4E9", "#009E73",
   "#F0E442", "#0072B2", "#D55E00", "#CC79A7"
 )
 
-amz_dat <- filter(dat$fam, study == "This")
+amz_dat <- filter(fam, study == "This")
 amz_pca <- pca(dat, amz_dat$sample)
 
 pca_plot <- amz_dat %>%
@@ -31,15 +36,16 @@ pca_plot <- amz_dat %>%
 pve_plot <- plot_pve(amz_pca)
 
 save_plot(
-  here("figs", "sup-fig", "sup-fig_amz-pca"),
+  here("figs", "sup-fig", "sup-fig4_amz-pca"),
   pca_plot / pve_plot +
-    plot_layout(heights = c(3,1)) +
+    plot_layout(heights = c(3, 1)) +
     plot_annotation(tag_levels = "A"),
   height = 10, width = 8
 )
 
 ## Native America PCA
-nat_dat <- filter(dat$fam, group == "NAT", age == "Contemporan")
+cli::cli_alert_info("Generating Sup Figure 5 - Contemporan Native American")
+nat_dat <- filter(fam, group == "NAT", age == "Contemporan")
 nat_pca <- pca(dat, nat_dat$sample)
 
 pca_plot <- nat_dat %>%
@@ -49,15 +55,16 @@ pca_plot <- nat_dat %>%
 pve_plot <- plot_pve(nat_pca)
 
 save_plot(
-  here("figs", "sup-fig", "sup-fig_nat-pca"),
+  here("figs", "sup-fig", "sup-fig5_nat-pca"),
   pca_plot / pve_plot +
-    plot_layout(heights = c(3,1)) +
+    plot_layout(heights = c(3, 1)) +
     plot_annotation(tag_levels = "A"),
   height = 10, width = 8
 )
 
 ## Global PCA
-global_dat <- filter(dat$fam, age == "Contemporan")
+cli::cli_alert_info("Generating Sup Figure 6 - Whole world")
+global_dat <- filter(fam, age == "Contemporan")
 global_pca <- pca(dat, global_dat$sample)
 
 pca_plot <- global_dat %>%
@@ -67,20 +74,11 @@ pca_plot <- global_dat %>%
 pve_plot <- plot_pve(global_pca)
 
 save_plot(
-  here("figs", "sup-fig", "sup-fig_global-pca"),
+  here("figs", "sup-fig", "sup-fig6_global-pca"),
   pca_plot / pve_plot +
-    plot_layout(heights = c(3,1)) +
+    plot_layout(heights = c(3, 1)) +
     plot_annotation(tag_levels = "A"),
   height = 10, width = 8
 )
 
-# ## Save pca
-# list(
-#   amz = amz_dat %>%
-#     bind_cols(as_tibble(amz_pca$projection)),
-#   nat = nat_dat %>%
-#     bind_cols(as_tibble(nat_pca$projection)),
-#   global = global_dat %>%
-#     bind_cols(as_tibble(global_pca$projection))
-#   ) %>%
-#   saveRDS(here("out", "pca.rds"))
+cli::cli_alert_success("Done!")
