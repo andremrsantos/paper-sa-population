47dd3e29de4830bb899251445e6f95d5c1f80df1
André Ribeiro dos Santos <andremr@MacBook-Pro.local>
2020-09-17 15:23:01 -0400

    rename run-qpGraph and added CLI

--- bin/plot-sup-fig_qpGraph.R
+++ bin/plot-sup-fig_qpGraph.R
@@ -1,11 +1,21 @@
-library(tidyverse)
-library(here)
-library(tidygraph)
-library(admixtools)
+cli::cli_h1("Plotting Sup. Figure 8-10 - qpGraph")
+cli::cli_alert_info("Setup environment")
 
-source(here("R", "setup.R"))
-source(here("R", "plink.R"))
-source(here("R", "sample.R"))
+suppressMessages({
+  library(tidyverse)
+  library(here)
+  library(tidygraph)
+  library(ggraph)
+  library(patchwork)
+  library(ggbeeswarm)
+  library(admixtools)
+
+  source(here("R", "setup.R"))
+  source(here("R", "plink.R"))
+  source(here("R", "sample.R"))
+})
+
+dir.create(here("figs", "sup-fig"), showWarnings = FALSE)
 
 ## Functions -----
 admixtree_layout <- function(edge) {
@@ -18,7 +28,6 @@ admixtree_layout <- function(edge) {
   layout <-
     tbl_graph(nodes = bind_rows(extd, node), edges = edge) %>%
     create_layout("tree")
-  
   admix_node <- filter(edge, type == "admix") %>%
     mutate(fy = layout$y[match(from, layout$name)]) %>%
     group_by(to) %>%
@@ -51,7 +60,9 @@ plot_admixtree <- function(layout, ...) {
       labels = c(admix = "Admixture", edge = "Normal"),
       limits = c("edge", "admix")
     ) +
-    scale_color_manual(values = subgroups_pal, guide = "none", na.value = "darkgray") +
+    scale_color_manual(
+      values = subgroups_pal, guide = "none", na.value = "darkgray"
+    ) +
     theme(legend.position = c(0, 1), legend.justification = c(0, 1)) +
     coord_cartesian(clip = "off")
 }
@@ -63,7 +74,7 @@ add_leaves <- function(model, leaves) {
 }
 
 ## Setup -----
-dir.create(here("figs", "sup-fig"), showWarnings = FALSE)
+cli::cli_alert_info("Load data")
 
 data_prefix <- here("out", "plink", "DataB")
 data_f2_dir <- here("out", "qpF2")
@@ -72,25 +83,29 @@ fam <- read_fam(paste0(data_prefix, ".fam")) %>%
   left_join(read_sample_info()) %>%
   filter(
     age == "Contemporan",
-    group == "NAT" | pop %in% c("Mbuti", "Han")
+    group %in% c("NAT", "OCE") | pop %in% c("Mbuti", "Han")
   )
 
 ## Define Models --------
 node <- tibble(
-  name = c("Root", "SA1", "SA2", "SA3", "Out", "WestAndes", "Amazon", "South", "SouthEast"),
+  name = c(
+    "Root", "SAB", "SA1", "SA2", "SA3", "Out",
+    "WestAndes", "Amazon", "South", "SouthEast"
+  ),
   region = name
 )
+
 models <- list(
   model1 = tribble(
     ~ from, ~ to, ~ type,
     "Root", "Out", "edge",
-    "Root", "SA1", "edge",
+    "Root", "SAB", "edge",
+    "SAB", "SA1", "edge",
+    "SAB", "SA2", "edge",
     "SA1", "South", "edge",
-    "SA1", "SA2", "edge",
-    "SA2", "WestAndes", "edge",
+    "SA1", "WestAndes", "edge",
     "SA2", "Amazon", "edge",
-    "Amazon", "SouthEast", "admix",
-    "WestAndes", "SouthEast", "admix",
+    "SA2", "SouthEast", "edge",
   ),
   model2 = tribble(
     ~ from, ~ to, ~type,
@@ -98,19 +113,19 @@ models <- list(
     "Root", "SA1", "edge",
     "SA1", "South", "edge",
     "SA1", "SA2", "edge",
-    "SA2", "WestAndes", "edge",
+    "SA2", "SA3", "edge",
     "SA2", "Amazon", "edge",
-    "Amazon", "SouthEast", "admix",
-    "South", "SouthEast", "admix",
+    "SA3", "WestAndes", "edge",
+    "SA3", "SouthEast", "edge",
   ),
   model3 = tribble(
     ~ from, ~ to, ~type,
     "Root", "Out", "edge",
     "Root", "SA1", "edge",
-    "SA1", "South", "edge",
     "SA1", "SA2", "edge",
-    "SA2", "WestAndes", "edge",
+    "SA1", "WestAndes", "edge",
     "SA2", "SA3", "edge",
+    "SA2", "South", "edge",
     "SA3", "Amazon", "edge",
     "SA3", "SouthEast", "edge",
   )
@@ -119,27 +134,31 @@ models <- list(
 ## Pre-Compute model likelihood ----
 model_dat_file <- here("out", "model_qpgraph.rds")
 if (!file.exists(model_dat_file)) {
-  library(furrr)
-  plan(multisession, workers = 4)
-  
+  cli::cli_alert_info("Running qpGraph")
+  suppressMessages({
+    library(furrr)
+    plan(multisession, workers = 4)
+  })
   ## Define populations to investigate
   pop_subset <- unique(fam$pop)
   pop_group <- distinct(fam, pop, subgroup) %>% with(split(pop, subgroup))
   pop_combn <- list(
     Out = c("Mbuti", "Han", "Pima", "Mayan"),
-    WestAndes = pop_group[['West Andes']],
-    Amazon = pop_group[['Amazon']],
-    South = pop_group[['South']],
-    SouthEast = pop_group[['South East']]
+    WestAndes = pop_group[["West Andes"]],
+    Amazon = pop_group[["Amazon"]],
+    South = pop_group[["South"]],
+    SouthEast = pop_group[["South East"]]
   ) %>%
-    cross()
-  
+  cross()
+
   ## Pre-compute F2
   if (!dir.exists(data_f2_dir)) {
-    extract_f2(data_prefix, data_f2_dir, pops = pop_subset)
+    cli::cli_alert_info("Running F2 computation")
+    extract_f2(data_prefix, data_f2_dir, pops = pop_subset, overwrite = TRUE)
   }
   data_f2 <- f2_from_precomp(data_f2_dir)
-  
+
+  cli::cli_alert_info("Fitting qpGraph models")
   dat <- list(combn = pop_combn, model_name = names(models)) %>%
     cross_df() %>%
     mutate(
@@ -155,10 +174,7 @@ if (!file.exists(model_dat_file)) {
 dat <- readRDS(model_dat_file)
 
 ## Plots ----
-library(ggraph)
-library(patchwork)
-library(ggbeeswarm)
-
+cli::cli_alert_info("Generating Sup Figure 8 - Models overview")
 model_plots <- models %>%
   map(tbl_graph, nodes = node) %>%
   map(create_layout, layout = "tree") %>%
@@ -166,17 +182,17 @@ model_plots <- models %>%
   wrap_plots() +
   plot_annotation(tag_prefix = "Model ", tag_levels = "1") +
   plot_layout(guides = "collect")
-
 save_plot(
-  here("figs", "sup-fig", "sup-fig_qpgraph_models"),
+  here("figs", "sup-fig", "sup-fig8_qpgraph_models"),
   model_plots, width = 8, height = 3
 )
 
+cli::cli_alert_info("Generating Sup Figure 9 - Top 10 Models")
 top_models <- dat %>%
   mutate(
-    score = map_dbl(graph, "score"),
+    score = map_dbl(graph, "worst_residual"),
     edges = map(graph, "edges"),
-    name = sprintf("%s - Likelihood: %.2f", model_name, score)
+    name = sprintf("%s - Worst F4: %.2f", model_name, score)
   ) %>%
   slice_min(score, n = 10)
 
@@ -190,33 +206,29 @@ top_models_plot <- top_models$edges %>%
   theme(legend.position = "bottom")
 
 save_plot(
-  here("figs", "sup-fig", "sup-fig_qpgraph_top_models"),
+  here("figs", "sup-fig", "sup-fig9_qpgraph_top_models"),
   top_models_plot, width = 10, height = 8
 )
 
-model_lik <- dat %>%
+cli::cli_alert_info("Generating Sup Figure 10 - Models score")
+plot_score <- function(dat, score = "score") {
+  dat %>%
   mutate(Out = parse_factor(Out, c("Mbuti", "Han", "Mayan", "Pima"))) %>%
-  mutate(score = map_dbl(graph, "score")) %>%
-  ggplot(aes(model_name, score)) +
-  geom_quasirandom(aes(color = model_name), show.legend = FALSE) +
-  stat_summary(fun.data = median_hilow) +
-  facet_grid(Out~., labeller = label_both) +
-  scale_x_discrete("") +
-  scale_y_log10("Likelihood score") +
-  scale_color_brewer(palette = "Set1") +
-  coord_flip() +
-  theme(strip.background.y = element_blank(), strip.text.y = element_blank())
-model_f4 <- dat %>%
-  mutate(Out = parse_factor(Out, c("Mbuti", "Han", "Mayan", "Pima"))) %>%
-  mutate(score = map_dbl(graph, "worst_residual")) %>%
+  mutate(score = map_dbl(graph, score)) %>%
   ggplot(aes(model_name, abs(score))) +
   geom_quasirandom(aes(color = model_name), show.legend = FALSE) +
   stat_summary(fun.data = median_hilow) +
-  facet_grid(Out~., labeller = label_both) +
+  facet_grid(Out ~ ., labeller = label_both) +
   scale_x_discrete("") +
-  scale_y_log10("Worst Absolute F4") +
   scale_color_brewer(palette = "Set1") +
   coord_flip() +
+  theme(strip.background.y = element_blank(), strip.text.y = element_blank())
+}
+
+model_lik <- plot_score(dat) +
+  scale_y_log10("Likelihood score")
+model_f4  <- plot_score(dat, "worst_residual") +
+  scale_y_log10("Worst Absolute F4") +
   theme(
     axis.line.y = element_blank(),
     axis.text.y = element_blank(),
@@ -225,6 +237,8 @@ model_f4 <- dat %>%
   )
 
 save_plot(
-  here("figs", "sup-fig", "sup-fig_qpgraph_model_likelihood"),
+  here("figs", "sup-fig", "sup-fig10_qpgraph_model_likelihood"),
   model_lik + model_f4, width = 8, height = 4
 )
+
+cli::cli_alert_success("Done!")
