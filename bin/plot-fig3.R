a605e82d828effdb1efd4fc7528b6fd1f22eeac7
André Ribeiro dos Santos <andremr@MacBook-Pro.local>
2020-09-16 23:39:23 -0400

    split plot-fig3-4 into separate file and added CLI

---
+++ bin/plot-fig3.R
@@ -0,0 +1,80 @@
+cli::cli_h1("Plotting Figure 3")
+## Setup environment -----
+cli::cli_alert_info("Setup environment")
+
+suppressMessages({
+  library(tidyverse)
+  library(here)
+  library(ggbeeswarm)
+
+  source(here("R", "setup.R"))
+  source(here("R", "plink.R"))
+  source(here("R", "sample.R"))
+  source(here("R", "dstat.R"))
+})
+
+dir.create(here("figs", "fig3"), recursive = TRUE, showWarnings = FALSE)
+
+## Load data -----
+cli::cli_alert_info("Loading data")
+
+plink <- read_plink(here("out", "plink", "DataB"))
+fam <- plink$fam %>% left_join(read_sample_info())
+pop <- fam %>%
+  group_by(pop, group, subgroup, age) %>%
+  summarise(across(c(lat, long), mean))
+
+order <- c(
+  "AKW", "ARA", "ARW", "AST", "AWA", "KAY", "PTJ", "WPI", "Surui", "Karitiana",
+  "Jabuticabeira", "LapadoSanto", "Laranjal", "Sumidouro"
+)
+
+## Computing statistics -----
+cli::cli_alert_info("Computing D-statistics for austrolasian signal")
+
+australasian <- unique(filter(fam, group == "OCE", pop != "Hawaiian")$pop)
+native       <- unique(filter(fam, group == "NAT", country == "Brazil")$pop)
+
+dstat_australasian_file <- here("out", "dstat-australasian.rds")
+if (!file.exists(dstat_australasian_file)) {
+  freq <- fam %>%
+    filter(pop %in% c(australasian, native, "Mbuti", "Mixe")) %>%
+    with(split(sample, pop)) %>%
+    discard(~ length(.x) == 0) %>%
+    compute_frequency(plink = plink)
+
+  dstat_australasian <- dstat(
+    "Mbuti", australasian, "Mixe", native, freq, size = 100
+  )
+  saveRDS(dstat_australasian, dstat_australasian_file)
+}
+dstat_australasian <- readRDS(dstat_australasian_file)
+
+## Plotting
+cli::cli_alert_info("Generating Figure 3")
+
+ylab <- c(
+  "D(Mbuti, Australasin; Mixe, X)\n",
+  "(Mixe, Australasian)<--->(X, Australasian)"
+)
+
+dstat_scatter <- dstat_australasian %>%
+  left_join(pop, by = c(z = "pop")) %>%
+  plot_dstat_scatter(z) +
+  labs(y = ylab, color = "Age") +
+  scale_x_discrete(limits = rev(order)) +
+  scale_y_continuous(breaks = c(-10, -5, -2, 0, 2, 5, 10)) +
+  coord_flip(ylim = c(-10, 10)) +
+  theme(
+    legend.position = c(1, 0),
+    legend.justification = c(1, 0),
+    legend.key.height = unit(1, "lines"),
+    legend.key.width = unit(.5, "lines"),
+    legend.background = element_blank()
+  )
+
+save_plot(
+  here("figs", "fig3", "fig3_dstat-australesian"),
+  dstat_scatter, width = 4, height = 4.5
+)
+cli::cli_alert_success("Done!")
---
+++ bin/plot-fig4.R
@@ -0,0 +1,93 @@
+cli::cli_h1("Plotting Figure 4")
+## Setup environment -----
+cli::cli_alert_info("Setup environment")
+
+suppressMessages({
+  library(tidyverse)
+  library(here)
+  library(ggbeeswarm)
+
+  source(here("R", "setup.R"))
+  source(here("R", "plink.R"))
+  source(here("R", "sample.R"))
+  source(here("R", "dstat.R"))
+  source(here("R", "map.R"))
+})
+
+dir.create(here("figs", "fig4"), recursive = TRUE, showWarnings = FALSE)
+dir.create(here("figs", "sup-fig"), recursive = TRUE, showWarnings = FALSE)
+
+
+pluck_pop <- function(dat, ...) {
+  filter(dat, ...) %>% pull(pop) %>% unique()
+}
+
+## Load data ----
+cli::cli_alert_info("Loading data")
+
+plink <- read_plink(here("out", "plink", "DataB"))
+fam <- plink$fam %>% left_join(read_sample_info())
+pop <- fam %>%
+  group_by(pop, group, subgroup, age) %>%
+  summarise(across(c(lat, long), mean))
+
+## Compute migration models ------
+cli::cli_alert_info("Compute D-statistics models")
+
+native <- pluck_pop(fam, group == "NAT")
+amazon <- pluck_pop(fam, subgroup == "Amazon", age == "Contemporan")
+wandes <- pluck_pop(fam, subgroup == "West Andes", age == "Contemporan")
+south  <- pluck_pop(fam, subgroup == "South", age == "Contemporan")
+
+dstat_models_file <- here("out", "dstat-model.rds")
+if (!file.exists(dstat_models_file)) {
+  freq <- with(fam, split(sample, pop)) %>%
+    discard(~ length(.x) == 0) %>%
+    compute_frequency(plink = plink)
+
+  dstat_models <- list(
+    model1 = dstat("Mbuti", native, amazon, wandes, freq),
+    model2 = dstat("Mbuti", native,  south, wandes, freq),
+    model3 = dstat("Mbuti", native, amazon,  south, freq)
+  )
+  saveRDS(dstat_models, dstat_models_file)
+}
+dstat_models <- readRDS(dstat_models_file)
+
+## Plots -----
+cli::cli_alert_info("Generating plots")
+
+## Fig4. D-stat IDW ------
+dstat_idw <- dstat_models %>%
+  set_names(c(
+    "(A) D(Mbuti,X;Amazon,West Andes)\n(X, Amazon) <---> (X, West Andes)",
+    "(B) D(Mbuti,X;South,West Andes)\n(X, South) <---> (X, West Andes)",
+    "(C) D(Mbuti,X;Amazon,South)\n(X, Amazon) <---> (X, South)"
+  )) %>%
+  map(mutate, zv = pmax(pmin(zv, 10), -10)) %>% # Cropping at [-10, 10]
+  map(left_join, pop, by = c(x = "pop")) %>%
+  idw_america()
+
+dstat_idw_map <- ggplot(pop) +
+  geom_raster(data = dstat_idw, aes(x, y, fill = var1.pred)) +
+  base_america_map(color = "darkgray", fill = NA) +
+  geom_point(aes(long, lat, shape = age)) +
+  scale_fill_distiller("Z-scaled D", palette = "RdYlBu") +
+  guides(
+    shape = guide_legend(
+      title.position = "top", direction = "vertical", order = 1
+    ),
+    color = guide_colorbar(title.position = "top", order = 2),
+    fill = guide_colorbar(title.position = "top", order = 2)
+  ) +
+  facet_grid(~Desc) +
+  theme(strip.background = element_blank())
+
+## Save Figure 4
+cli::cli_alert_info("Saving Figure 4")
+
+save_plot(
+  here("figs", "fig4", "fig4_dstat-idw-map"),
+  dstat_idw_map, width = 8, height = 4
+)
+cli::cli_alert_success("Done!")
