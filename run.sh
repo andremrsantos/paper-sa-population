4c742d3f5342d06b673823c811b5b002b249ab50
André Ribeiro dos Santos <andremr@MacBook-Pro.local>
2020-09-17 16:46:39 -0400

    updated path to scripts and added comments

--- run.sh
+++ run.sh
@@ -1,10 +1,16 @@
+#! /bin/bash -e
+
+## It requires out/admixture files and out/plink/DataB (final dataset
+## without related and admixed samples).
+
+## Generate main figures
 Rscript --vanilla bin/plot-fig1.R
 Rscript --vanilla bin/plot-fig2.R
-Rscript --vanilla bin/plot-fig3-4.R
-
-Rscript --vanilla bin/plot-full-admixture.R
-Rscript --vanilla bin/plot-PCAs.R
-Rscript --vanilla bin/run-f3.R
-Rscript --vanilla bin/run-qpGraph.R
-Rscript --vanilla bin/run-ROH.R
-Rscript --vanilla bin/run-treemix.R
+Rscript --vanilla bin/plot-fig3.R
+Rscript --vanilla bin/plot-fig4.R
+## Generate supplementary material
+Rscript --vanilla bin/plot-sup-fig_admixture.R
+Rscript --vanilla bin/plot-sup-fig_treemix.R
+Rscript --vanilla bin/plot-sup-fig_PCA.R
+Rscript --vanilla bin/plot-sup-fig_ROH.R
+Rscript --vanilla bin/plot-sup-fig_qpGraph.R
