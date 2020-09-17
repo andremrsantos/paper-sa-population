c45b19fbcf561d032ca663c94cf8c5d41fab26ce
André Ribeiro dos Santos <andremr@MacBook-Pro.local>
2020-09-16 23:54:43 -0400

    rename plot-full-admixture and add CLI

--- bin/plot-sup-fig_admixture.R
+++ bin/plot-sup-fig_admixture.R
@@ -1,27 +1,25 @@
-library(tidyverse)
-library(here)
+cli::cli_h1("Plotting Sup. Figure 1 - Admixture")
+## Setup environment -----
+cli::cli_alert_info("Setup environment")
 
-source(here("R", "setup.R"))
-source(here("R", "sample.R"))
-source(here("R", "plink.R"))
-source(here("R", "admixture.R"))
+suppressMessages({
+  library(tidyverse)
+  library(here)
 
-strip_clip_off <- function(plot, strip = c("r", "l", "t", "b")) {
-  strip <- paste0("strip-", match.arg(strip))
-  q <- ggplotGrob(plot)
-  for (i in which(grepl(strip, q$layout$name)))
-    q$grobs[[i]]$layout$clip <- "off"
-  patchwork::wrap_elements(q)
-}
+  source(here("R", "setup.R"))
+  source(here("R", "sample.R"))
+  source(here("R", "plink.R"))
+  source(here("R", "admixture.R"))
+})
 
-## Setup
 dir.create(here("figs", "sup-fig"), recursive = TRUE, showWarnings = FALSE)
 
-## Load data
+## Load data -----
+cli::cli_alert_info("Loading data")
+
 fam <- here("out", "plink", "DataA.fam") %>%
   read_fam() %>%
   left_join(read_sample_info())
-
 dat <- tibble(Ks = 2:10, K = factor(Ks, labels = sprintf("K=%d", Ks))) %>%
   mutate(
     q = here("out", "admixture", "DataA.%d.Q") %>%
@@ -34,25 +32,12 @@ dat <- tibble(Ks = 2:10, K = factor(Ks, labels = sprintf("K=%d", Ks))) %>%
   unnest(c(q, log))
 
 ## plot full distruct
-distruct <- plot_distruct(select(dat, -Ks), pop, subgroup2, K = K) +
+cli::cli_alert_info("Generating Sup Figure 1 - Admixture")
+distruct_plot <-
+  plot_distruct(select(dat, -Ks), pop, subgroup2, K = K) +
   scale_fill_manual(values = cbpal) +
-  facet_grid(K~subgroup2, scales = "free", space = "free")
-
-full_distruct <- distruct +
-  facet_grid(K~subgroup2, scales = "free", space = "free") +
-  theme(
-    axis.text.x = element_text(angle = 90, hjust = 1, size = 5),
-    strip.text.x = element_text(angle = 90, hjust = 0, size = 8),
-    strip.background.x = element_blank()
-  )
-save_plot(
-  here("figs", "sup-fig", "sup-fig_admixture-full"),
-  strip_clip_off(full_distruct, "t"), width = 10, height = 8
-)
-
-vert_distruct <- distruct +
-  facet_grid(subgroup2~K, scales = "free", space = "free") +
   coord_flip() +
+  facet_grid(subgroup2~K, scales = "free", space = "free") +
   theme(
     axis.text.y = element_text(size = 5),
     strip.text.y = element_text(angle = 0, hjust = 0, size = 8),
@@ -60,10 +45,11 @@ vert_distruct <- distruct +
     panel.spacing.x = unit(.4, "lines")
   )
 save_plot(
-  here("figs", "sup-fig", "sup-fig_admixture-vertical"),
-  strip_clip_off(vert_distruct, "r"), width = 8, height = 11
+  here("figs", "sup-fig", "sup-fig1_admixture"),
+  strip_clip_off(distruct_plot, "r"), width = 8, height = 11
 )
 
+cli::cli_alert_info("Generating Sup Figure 3 - Admixture Diagnostics")
 full_diagnostic <- dat %>%
   distinct(Ks, cv_error, likelihood) %>%
   rename(`CV Error` = cv_error, Likelihood = likelihood) %>%
@@ -79,8 +65,9 @@ full_diagnostic <- dat %>%
     strip.background = element_blank(),
     strip.placement = "outside"
   )
-
 save_plot(
-  here("figs", "sup-fig", "sup-fig_admixture-diagnostic"),
+  here("figs", "sup-fig", "sup-fig3_admixture-diagnostic"),
   full_diagnostic, width = 4, height = 4
 )
+
+cli::cli_alert_success("Done!")
